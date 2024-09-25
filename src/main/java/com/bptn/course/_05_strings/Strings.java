b274b59255692ee6be2e0f65054820d2fd35265e
mohammed <m.ahmednwq@gmail.com>
2024-09-25 15:59:48 -0400

    Updating String.java file

--- src/main/java/com/bptn/course/_05_strings/Strings.java
+++ src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -1,8 +1,43 @@
 package com.bptn.course._05_strings;
 
+import java.util.Scanner;
+
 public class Strings {
 
 	public static void main(String[] args) {
-		
+
+		String userName = "   testuser";
+//		System.out.println(userName.length());
+//		System.out.println(userName.charAt(2));
+//		System.out.println(userName.substring(4));
+//		System.out.println(userName.substring(2,4));
+//		System.out.println(userName.equals("testusr"));
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Write down a name: ");
+		String input = scanner.nextLine();
+		input.trim();
+		input.toLowerCase();
+
+		int vowel = 0;
+		int constant = 0;
+
+		for (int i = 0; i < input.length(); i++) {
+			char ch = input.charAt(i);
+
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				vowel++;
+
+			} else if (ch >= 'a' && ch <= 'z') {
+
+				constant++;
+			}
+
+		}
+
+		System.out.println("The number of vowels are: " + vowel);
+		System.out.println("The number of constants are: " + constant);
+
 	}
 }
